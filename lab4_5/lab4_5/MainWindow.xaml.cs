f70a4147aa95dfc7eb41b0afbb8ef484c9c1dd38
Ekaterina <zhuchkevich.eka@gmail.com>
2025-04-22 03:11:55 +0300

    почти 4-5 сделала

--- lab4_5/lab4_5/MainWindow.xaml.cs
+++ lab4_5/lab4_5/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using System.Collections;
+using System.Reflection;
 
 namespace lab4_5
 {
@@ -27,9 +28,13 @@ namespace lab4_5
 
         List<Product> sortedProducts = new List<Product>();
 
+        List<Product> foundProducts = new List<Product>();
+
         public MainWindow()
         {
             InitializeComponent();
+            //Cursor cursor = new Cursor("D:\\лабораторные работы\\ооп\\lab4_5\\pics\\klipartz.com.cur");
+            //this.Cursor = cursor;
             LoadProducts(products);
         }
         public void LoadProducts(List<Product> products)
@@ -83,10 +88,23 @@ namespace lab4_5
                     BorderThickness = new Thickness(0)
                 };
 
+                var buttonDel = new Button
+                {
+                    Content = "Удалить",
+                    Margin = new Thickness(5, 1, 5, 5),
+                    Background = Brushes.Red,
+                    Foreground = Brushes.White,
+                    Height = 20,
+                    BorderThickness = new Thickness(0),
+                    Tag = product
+                };
+                buttonDel.Click += DelProduct_Click;
+
                 stack.Children.Add(image);
                 stack.Children.Add(description);
                 stack.Children.Add(name);
                 stack.Children.Add(button);
+                stack.Children.Add(buttonDel);
 
                 border.Child = stack;
 
@@ -99,7 +117,7 @@ namespace lab4_5
         {
             e.Handled = !IsTextNumeric(e.Text);
         }
-            
+
         private void NumberOnly_Pasting(object sender, DataObjectPastingEventArgs e)
         {
             if (e.DataObject.GetDataPresent(typeof(string)))
@@ -123,7 +141,7 @@ namespace lab4_5
         private void Adjust_Click(object sender, RoutedEventArgs e)
         {
             var sorted = SortProduct();
-            CatalogPanel.Children.Clear(); 
+            CatalogPanel.Children.Clear();
             LoadProducts(sortedProducts);
         }
         public List<Product> SortProduct()
@@ -139,6 +157,103 @@ namespace lab4_5
                 .ToList();
             sortedProducts.AddRange(sorted);
             return sortedProducts;
+        }
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            var sorted = SortProduct();
+            CatalogPanel.Children.Clear();
+
+            FromTB.Clear();
+            ToTB.Clear();
+
+            if (Brand.Items.Count > 0)
+            {
+                Brand.SelectedIndex = 0;
+            }
+
+            LoadProducts(products);
+        }
+
+        //poisk pisek
+        public List<Product> SearchProduct()
+        {
+            foundProducts.Clear();
+            string searchCriteria = SearchBox.Text;
+
+            if (string.IsNullOrEmpty(searchCriteria))
+            {
+                foundProducts.AddRange(products);
+            }
+
+            else
+            {
+                var searchLower = searchCriteria.ToLower();
+                var filtered = products
+                    .Where(p => p.Name.ToLower().Contains(searchLower))
+                    .ToList();
+                foundProducts.AddRange(filtered);
+            }
+            return foundProducts;
+        }
+        public void Search_Click(object sender, RoutedEventArgs e)
+        {
+            var results = SearchProduct();
+            CatalogPanel.Children.Clear();
+            LoadProducts(results);
+        }
+
+        public void SearchBox_TextChanged(object sender, RoutedEventArgs e)
+        {
+            var results = SearchProduct();
+            CatalogPanel.Children.Clear();
+            LoadProducts(results);
+        }
+
+        //lang
+        private string currentLanguage = "ru";
+        private void ChangeLanguage_Click(object sender, RoutedEventArgs e)
+        {
+            string newLang = currentLanguage == "ru" ? "eng" : "ru";
+            string newDictPath = $"D:\\лабораторные работы\\ооп\\lab4_5\\Resources\\Resources.{newLang}.xaml";
+
+            var dicts = Application.Current.Resources.MergedDictionaries;
+            dicts.Clear();
+
+            var newDict = new ResourceDictionary
+            {
+                Source = new Uri(newDictPath, UriKind.Absolute)
+            };
+            dicts.Add(newDict);
+
+            currentLanguage = newLang;
+        }
+
+        //manage products
+        public void DelProduct_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var productToRemove = button?.Tag as Product;
+
+            if (productToRemove != null)
+            {
+                var result = MessageBox.Show("Вы точно хотите удалить этот товар?", "Подтверждение удаления", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    products.Remove(productToRemove); 
+                    CatalogPanel.Children.Clear();
+                    LoadProducts(products);          
+                }
+            }
+        }
+
+        public void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public void AddProduct(Product product)
+        {
+
         }
     }
 }
